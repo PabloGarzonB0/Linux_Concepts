2e9e21eda6b507aa3b1d88d06c4d868fc6454e82
PabloGarzón <79424389+PabloGarzonB0@users.noreply.github.com>
2025-11-11 22:38:50 -0500

    Improve features into the code, code ended

--- Comandos_Linux/Shell_Scripting_tasks/Ejercicios_Bash/WebSiteStatic.ps1
+++ Comandos_Linux/Shell_Scripting_tasks/Ejercicios_Bash/WebSiteStatic.ps1
@@ -5,16 +5,31 @@ En el siguiente script se ejecutan las siguientes actividades:
 - Establecer la contrasena del usuario administrador
 #>
 
+<powershell>
+
 # Installing web server
 Install-windowsFeature -name Web-Server -IncludeManagementTools
 
 # Getting website code
 wget hhttps://us-east-1-tcprod.s3.amazonaws.com/courses/CUR-TF-100-
 EDCOMP/v1.0.4.prod-ef70397c/01-Lab-ec2/scripts/code.zip -outfile
-"C:\Users\Administrator\Downloads\code.zip"
+"C:\Users\Administrator\Downloads\code.zip"   # It's necessary change the root path
 
 # Unzipping website code
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
+function Unzipping
+{
+    param([string]$zipfile, [string]$outpath)
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
 
+Unzip "C:\Users\Administrator\Downloads\code.zip" "C:\inetpub\"
+
+# Setting Administrator password
+$Secure_String_Pwd = ConvertTo-SecureString "P@ssWOrD!" -AsPlainText -Force
+$UserAccount = Get-LocalUser -Name "Administrator"
+$UserAccount | Set-LocalUser -Password $Secure_String_Pwd
+
+</powershell>
 
